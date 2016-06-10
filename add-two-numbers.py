6f3ae624663105f90169f65757453ebec855e4dd
sz11 <szsxt818@gmail.com>
2016-06-10 16:22:33 +0800

    add-two-numbers-write

--- add-two-numbers.py
+++ add-two-numbers.py
@@ -13,33 +13,33 @@ class Solution(object):
         """
         ret = ListNode(0)
         ans = ret
+        carry = 0
         while(l1 != None and l2 != None):
-            tmp = l1.val + l2.val
-            ret.val += tmp % 10
+            tmp = l1.val + l2.val + carry
+            ret.next = ListNode(tmp % 10)
             l1 = l1.next
             l2 = l2.next
-            ret.next = ListNode(tmp / 10)
+            carry = tmp / 10
             ret = ret.next
         while(l1 != None):
-            ret.val += l1.val % 10
-            ret.next = ListNode(l1.val / 10)
+            tmp = l1.val + carry
+            ret.next = ListNode(tmp % 10)
             l1 = l1.next
+            carry = tmp / 10
             ret = ret.next
         while(l2 != None):
-            ret.val += l2.val % 10
-            ret.next = ListNode(l2.val / 10)
+            tmp = l2.val + carry
+            ret.next = ListNode(tmp % 10)
             l2 = l2.next
+            carry = tmp / 10
             ret = ret.next
-        if ret.val == 0:
-            ret = None
-        return ans
+        if carry != 0:
+            ret.next = ListNode(carry)
+            ret = ret.next
+        return ans.next
 
-l1 = ListNode(2)
-l1.next = ListNode(4)
-l1.next.next = ListNode(3)
+l1 = ListNode(5)
 l2 = ListNode(5)
-l2.next = ListNode(6)
-l2.next.next = ListNode(4)
 sol = Solution()
 ret = sol.addTwoNumbers(l1, l2)
 while(ret != None):
