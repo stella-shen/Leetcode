73178ddbc7f860cdf9c6f29fe6008267c749002a
Zhe Shen <szsxt818@gmail.com>
2016-07-23 15:31:57 +0800

    lps passed using manacher's algorithm

--- longest-palindromic-substring.py
+++ longest-palindromic-substring.py
@@ -1,31 +1,53 @@
 
 class Solution(object):
+    def preprocess(self, s):
+        """
+
+        :type s: str
+        :rtype: str
+        """
+        if s == "":
+            return "^$"
+        ret = "^"
+        for c in s:
+            ret += "#" + c
+        ret += "#$"
+        return ret
+
     def longestPalindrome(self, s):
         """
 
         :type s: str
         :rtype: str
         """
-        maxLen = 0
-        palin = dict()
-        start = 0
-        end = 0
-        for i in range(0, len(s)):
-            palin[(i, i)] = 1
-            for j in range(0, i):
-                palin[(j, i)] = 0
-        for i in range(0, len(s)):
-            for j in range(0, i):
-                if (s[i] == s[j]) and (i == j+1):
-                    palin[(j, i)] = 1
-                if i - 1 >= j + 1:
-                    if (s[i] == s[j]) and (palin[(j+1,i-1)]):
-                        palin[(j, i)] = 1
-                if (palin[(j, i)] == 1) and (maxLen < i-j+1):
-                    maxLen = i - j + 1
-                    start = j
-                    end = i
-        return s[start : end + 1]
+        ret = self.preprocess(s)
+        C = 0
+        R = 0
+        p = list()
+        for i in range(len(ret)):
+            p.append(0)
+        for i in range(1, len(ret) - 1):
+            mirror_i = 2 * C - i
+            if p[mirror_i] < R - i:
+                p[i] = p[mirror_i]
+            else:
+                p[i] = R - i
+                for j in range(p[i] + 1, len(ret)):
+                    if ret[i + j] == ret[i - j]:
+                        p[i] += 1
+                    else:
+                        break
+                cur_R = i + p[i]
+                if cur_R > R:
+                    C = i
+                    R = cur_R
+        center = 0
+        max_len = 0
+        for i in range(1, len(ret) - 1):
+            if p[i] > max_len:
+                max_len = p[i]
+                center = i
+        return s[(center-max_len+1)/2-1 : (center+max_len-1)/2]
 
 if __name__ == '__main__':
     sol = Solution()
