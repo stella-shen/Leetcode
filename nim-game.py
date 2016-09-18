a4d0d5ff0be38589a104e21a7ae26a5d7aac9256
Zhe Shen <szsxt818@gmail.com>
2016-09-19 00:01:39 +0800

    nim-game added

---
+++ nim-game.py
@@ -0,0 +1,16 @@
+
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n % 4 == 0:
+            return False
+        else:
+            return True
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.canWinNim(10)
+    
