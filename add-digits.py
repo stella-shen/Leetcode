49f442c935abfc96788e5cf4a70c6d62b9d181b7
Zhe Shen <szsxt818@gmail.com>
2016-09-19 00:31:21 +0800

    ad passed

---
+++ add-digits.py
@@ -0,0 +1,17 @@
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0:
+            return 0
+        elif num % 9 == 0:
+            return 9
+        else:
+            return num % 9
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.addDigits(283)
