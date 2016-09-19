aa819de7e2b2c757fb66c0b82927950d516eee50
Zhe Shen <szsxt818@gmail.com>
2016-09-19 11:02:06 +0800

    soti passed

---
+++ sum-of-two-integers.py
@@ -0,0 +1,18 @@
+
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        MAX_INT = 0x7FFFFFFF
+        MIN_INT = 0x80000000
+        MASK = 0x100000000
+        while b:
+            a, b = (a ^ b) % MASK, ((a & b) << 1) % MASK
+        return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.getSum(-1, 2)
