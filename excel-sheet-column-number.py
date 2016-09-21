9826ffdefe21f531bfd94a1340abd9d12140358d
Zhe Shen <szsxt818@gmail.com>
2016-09-21 12:22:51 +0800

    escn passed

---
+++ excel-sheet-column-number.py
@@ -0,0 +1,17 @@
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ret = 0
+        for i in xrange(len(s)):
+            ret *= 26
+            ret += ord(s[i]) - ord('A') + 1
+        return ret
+
+if __name__ == '__main__':
+    sol = Solution()
+    s = "AB"
+    print sol.titleToNumber(s)
