045e9b8f4664cc68692ea745a2063a019aef7318
stella-shen <szsxt818@gmail.com>
2017-05-09 10:40:13 +0800

    bttbassii passed

---
+++ best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,11 @@
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        res = 0
+        for i in range(1, len(prices)):
+            res += max(0, prices[i] - prices[i - 1])
+        return res
