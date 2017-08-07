d18076a53f0bb2f66a77696d041aa987f893b285
stella-shen <szsxt818@gmail.com>
2017-08-07 16:44:44 +0800

    counting-bits.py passed

---
+++ counting-bits.py
@@ -0,0 +1,25 @@
+
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        if num == 0:
+            return [0]
+        ret = [0 for i in range(num + 1)]
+        ret[1] = 1
+        power_num = 1
+
+        for ind in range(2, num + 1):
+            if ind == (2 ** power_num):
+                ret[ind] = 1
+                power_num += 1
+            else:
+                ret[ind] = ret[(2 ** (power_num - 1))] + ret[ind - (2 ** (power_num - 1))]
+
+        return ret
+        
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.countBits(5)
