0a65ef32d7adabe8d72915290bd433f639cf4f13
stella-shen <szsxt818@gmail.com>
2017-08-08 12:06:24 +0800

    integer-break.py passed with O(n)

---
+++ integer-break.py
@@ -0,0 +1,16 @@
+
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 4:
+            return n - 1
+        res = [0, 1, 2, 3]
+        for i in xrange(4, n + 1):
+            res[i % 4] = max(res[(i - 2) % 4] * 2, res[(i - 3) % 4] * 3)
+        return res[n % 4]
+
+if __name__ == '__main__':
+    sol = Solution()
