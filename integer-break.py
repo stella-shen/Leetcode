c57ff90699ee1505eb460b0541e3e10867ef83e7
stella-shen <szsxt818@gmail.com>
2017-08-08 15:29:08 +0800

    integer passed with O(logn)

--- integer-break.py
+++ integer-break.py
@@ -7,10 +7,14 @@ class Solution(object):
         """
         if n < 4:
             return n - 1
-        res = [0, 1, 2, 3]
-        for i in xrange(4, n + 1):
-            res[i % 4] = max(res[(i - 2) % 4] * 2, res[(i - 3) % 4] * 3)
-        return res[n % 4]
+        res = 1
+        if n % 3 == 0:
+            res = 3 ** (n // 3)
+        if n % 3 == 1:
+            res = 3 ** (n // 3 - 1) * 4
+        if n % 3 == 2:
+            res = 3 ** (n // 3) * 2
+        return res
 
 if __name__ == '__main__':
     sol = Solution()
