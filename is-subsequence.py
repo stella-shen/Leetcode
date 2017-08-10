b8a7df7c148242d0ea1d6be111c1237711669158
stella-shen <szsxt818@gmail.com>
2017-08-10 15:30:58 +0800

    is-subsequence.py passed

---
+++ is-subsequence.py
@@ -0,0 +1,18 @@
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) == 0:
+            return True
+
+        i = 0
+        for c in t:
+            if c == s[i]:
+                i += 1
+            if i == len(s):
+                break
+        return i == len(s)
