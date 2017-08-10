3f3db77d10bf450ade1598c6d1c5faa3b12c3bd1
stella-shen <szsxt818@gmail.com>
2017-08-10 11:41:25 +0800

    time limit exceeded version

---
+++ predict-the-winner.py
@@ -0,0 +1,15 @@
+
+class Solution(object):
+    def PredictTheWinner(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return self.winner(nums, 0, len(nums) - 1, 1) >= 0
+        
+    def winner(self, nums, s, e, turn):
+        if s == e:
+            return turn * nums[s]
+        a = turn * nums[s] + self.winner(nums, s + 1, e, turn * (-1))
+        b = turn * nums[e] + self.winner(nums, s, e - 1, turn * (-1))
+        return turn * max(turn * a, turn * b)
