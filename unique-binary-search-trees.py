dafbbba61655acc965473b0d512769ae0edba357
stella-shen <szsxt818@gmail.com>
2017-08-12 23:30:38 +0800

    unique-binary-search-trees.py passed using catlan

--- unique-binary-search-trees.py
+++ unique-binary-search-trees.py
@@ -5,10 +5,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        res = [0] * (n + 1)
-        res[0] = 1
-        res[1] = 1
-        for i in xrange(2, n + 1):
-            for j in xrange(i):
-                res[i] += res[j] * res[i - j - 1]
-        return res[n]
+        if n == 0:
+            return 1
+
+        def combination(n, k):
+            res = 1
+            for i in xrange(k):
+                res *= (n - i)
+                res /= (i + 1)
+            return res
+
+        return combination(2 * n, n) - combination(2 * n, n - 1)
