14a9b59628d808c492627bbf6132adcba86f35e8
stella-shen <szsxt818@gmail.com>
2017-08-22 00:28:08 +0800

    using kth largest

--- wiggle-sort-ii.py
+++ wiggle-sort-ii.py
@@ -6,6 +6,54 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
-        median = (len(nums) - 1) // 2
-        nums[: : 2], nums[1: :2] = nums[median: : -1], nums[: median: -1]
+        def idx(i, n):
+            return (1 + 2 * i) % (n | 1)
+        
+        n = len(nums)
+        if n < 2:
+            return
+
+        median = self.findKthLargest(nums, n / 2)
+        i, j, k = 0, 0, n - 1
+        while j <= k:
+            if nums[idx(j, n)] > median:
+                nums[idx(i, n)], nums[idx(j, n)] = nums[idx(j, n)], nums[idx(i, n)]
+                i += 1
+                j += 1
+            elif nums[idx(j, n)] < median:
+                nums[idx(j, n)], nums[idx(k, n)] = nums[idx(k, n)], nums[idx(j, n)]
+                k -= 1
+            else:
+                j += 1
+
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        random.shuffle(nums)
+        while left <= right:
+            pivot = random.randint(left, right)
+            new_pivot = self.partition(nums, left, right, pivot)
+            if new_pivot == k - 1:
+                return nums[new_pivot]
+            elif new_pivot > k - 1:
+                right = new_pivot - 1
+            elif new_pivot < k - 1:
+                left = new_pivot + 1
+        
+    def partition(self, nums, left, right, pivot):
+        key = nums[pivot]
+        nums[pivot], nums[right] = nums[right], nums[pivot]
+        new_pivot = left
+        for i in xrange(left, right):
+            if nums[i] > key:
+                nums[new_pivot], nums[i] = nums[i], nums[new_pivot]
+                new_pivot += 1
+        nums[right], nums[new_pivot] = nums[new_pivot], nums[right]
+        return new_pivot
+
+if __name__ == '__main__':
+    Solution().wiggleSort([1, 3, 2, 2, 3, 1])
