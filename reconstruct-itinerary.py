2c4d085a90cf8fe68228475b8147a96dfd4a4cb6
Shen Zhe <szsxt818@gmail.com>
2017-09-22 15:29:59 +0800

    reconstruct-itinerary.py passed

---
+++ reconstruct-itinerary.py
@@ -0,0 +1,32 @@
+import collections
+
+class Solution(object):
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        def route_helper(origin, ticket_cnt, graph, ans):
+            if ticket_cnt == 0:
+                return True
+
+            for i, (dest, valid) in enumerate(graph[origin]):
+                if valid:
+                    graph[origin][i][1] = False
+                    ans.append(dest)
+                    if route_helper(dest, ticket_cnt - 1, graph, ans):
+                        return ans
+                    ans.pop()
+                    graph[origin][i][1] = True
+            return False
+
+        graph = collections.defaultdict(list)
+        for ticket in tickets:
+            graph[ticket[0]].append([ticket[1], True])
+        for k in graph.keys():
+            graph[k].sort()
+
+        origin = 'JFK'
+        ans = ['JFK']
+        route_helper(origin, len(tickets), graph, ans)
+        return ans
