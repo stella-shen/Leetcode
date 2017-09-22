b56b060dd115c2e6c16e0f8ffb014fd915493050
Shen Zhe <szsxt818@gmail.com>
2017-09-22 17:33:04 +0800

    clone-graph.py passed

---
+++ clone-graph.py
@@ -0,0 +1,23 @@
+# Definition for a undirected graph node
+class UndirectedGraphNode:
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
+
+class Solution:
+    # @param node, a undirected graph node
+    # @return a undirected graph node
+    def cloneGraph(self, node):
+        if node is None:
+            return None
+        cloned_node = UndirectedGraphNode(node.label)
+        cloned, queue = {node: cloned_node}, [node]
+        while queue:
+            cur = queue.pop()
+            for neighbor in cur.neighbors:
+                if neighbor not in cloned:
+                    queue.append(neighbor)
+                    cloned_neighbor = UndirectedGraphNode(neighbor.label)
+                    cloned[neighbor] = cloned_neighbor
+                cloned[cur].neighbors.append(cloned[neighbor])
+        return cloned[node]
